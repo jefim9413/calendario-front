1a7eb0c2c6c9122f76f4a5f174c314d51d5f7bd3
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-24 15:09:18 -0300

    corrigindo estilização

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,11 +6,11 @@ const Form = ()=> {
     const FormContainer = styled.form`
         display: flex;
         align-items: flex-end;
-        gap: 10px;
+        gap: 20px;
         flex-wrap: wrap;
         background-color: #fff;
         padding: 20px;
-        box-shadow: 0px 0px 0px #ccc;
+        box-shadow: 0px 0px 5px #ccc;
         border-radius: 5px;
     `
 
@@ -20,42 +20,44 @@ const Form = ()=> {
     `
 
     const Input = styled.input`
-        width: 120px;
-        padding: 0 10px;
-        border: 1px solid #bbb;
+        width: 100px;
+        padding: 8 10px;
+        border: 3px solid #bbb;
         border-radius: 5px;
-        height: 40px;
+        height: 35px;
     `
 
     const Label = styled.label``;
 
     const Button = styled.button`
-        padding: 10px;
+        display: flex;
+        margin: 0px auto;
+        padding: 17px;
         cursor: pointer;
         border-radius: 5px;
         border: none;
         background-color: #2c73d2;
         color: while;
-        height: 42px;
     `
 
     return (
         <FormContainer>
             <InputArea>
-                <Label>Title</Label>
+                <Label>Title </Label>
                 <Input name="title"/>
             </InputArea>
             <InputArea>
-                <Label>Description</Label>
+                <Label>Description </Label>
                 <Input name="description"/>
             </InputArea>
+          
             <InputArea>
-                <Label>Date</Label>
-                <Input name="date" type="date"/>
+                <Label>Duration </Label>
+                <Input name="duration"/>
             </InputArea>
             <InputArea>
-                <Label>Duration</Label>
-                <Input name="duration"/>
+                <Label>Date </Label>
+                <Input name="date" type="date"/>
             </InputArea>
             <Button type="submit"> Salvar </Button>
         </FormContainer>
