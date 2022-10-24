4b8e93196af7b81a002a76e462817e6f6a18ce74
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-24 16:26:32 -0300

    estilização de Table

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,5 +1,15 @@
 import styled from "styled-components";
 
+const Table = styled.table`
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+  max-width: 1120px;
+  margin: 20px auto;
+  word-break: break-all;
+`
 
 
 const Grid = () => {
