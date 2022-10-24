edb180e44d5bf2af217a38fef51083b7977e076f
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-24 14:35:42 -0300

    adicionando estilização do container

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import { useEffect, useState } from "react";
 import { Form } from "./components/Form";
+import styled from "styled-components";
+
+
+const Container = styled.div`
+  width: 100%;
+  max-width: 800px;
+  margin-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+`
+
+
 function App() {
   return (
-    <Form></Form>
+    <>
+    
+    
+    
+    </>
+
   );
 }
 
