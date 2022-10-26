21fb8d9adec94d2bfbf112d5dd589176990fde0b
Jefferson Marques <jefferson.jsm3444@gmail.com>
2022-10-25 21:22:38 -0300

    finalizando componente Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { toast } from "react-toastify";
 
@@ -50,34 +50,40 @@ const FormContainer = styled.form`
 
 
 const Form = ()=> {
-    
     const [task, setTask] = useState({
         title: "",
         description: "",
         duracao: "",
         date: ""
     })
+    const [status, setStatus] = useState({
+        type: '',
+        mensagem: ''
+      });
+    
 
     const evento = (e)=> {
         let nome = e.target.name;
         let valor = e.target.value;
         setTask({...task, [nome]: valor });
     }
-
+    
     const handleSubmit = async (e) => {
          e.preventDefault();
          const user = task;
-    
         if (
           !user.title ||
           !user.description ||
           !user.duracao||
           !user.date
         ) {
+            setStatus({
+                type: 'error',
+                mensagem: "Usuário não cadastrado com sucesso!"
+            });
           return toast.warn("Preencha todos os campos!");
         }
-       
-        const t =   await axios
+        await axios
             .post("http://localhost:3333/task", {
                 title: user.title,
                 description: user.description,
@@ -86,36 +92,42 @@ const Form = ()=> {
               })
               .then(({ data }) => toast.success(data)) 
               .catch(({ data }) => toast.error(data));
+              
     
         user.title= ""
         user.description=""
         user.duracao=""
         user.date=""
+        setTask(user)
+       
+        setStatus({
+            type: 'success',
+            mensagem: "Usuário cadastrado com sucesso!"
+        });
     }
     
     return (
         
-        <FormContainer onSubmit={handleSubmit}>
-             
+        <FormContainer onSubmit={handleSubmit }>
+            {status.type === 'success' ? <p style={{ color: "green" }}>{status.mensagem}</p> : ""}
+            {status.type === 'error' ? <p style={{ color: "#ff0000" }}>{status.mensagem}</p> : ""}
             <InputArea>
                 <Label>Title </Label>
-                <Input name="title" type="text"  onChange={evento} />
+                <Input name="title" type="text" value={task.title} onChange={evento} />
             </InputArea>
             <InputArea>
-                <Label> Description </Label>
-                <Input name="description" type="text"  onChange={evento}/>
+                <Label>Description </Label>
+                <Input name="description" type="text" value={task.description} onChange={evento}/>
             </InputArea>
-          
             <InputArea>
                 <Label>Duration </Label>
-                <Input name="duracao" type="number"  onChange={evento}/>
+                <Input name="duracao" type="number" value={task.duracao} onChange={evento}/>
             </InputArea>
             <InputArea>
                 <Label>Date </Label>
-                <Input name="date" type="date" onChange={evento}/>
+                <Input name="date" type="date" value={task.date} onChange={evento}/>
             </InputArea>
             <Button type="submit"> Salvar </Button>
-           
         </FormContainer>
         
     )
